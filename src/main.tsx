4e8447459bf573dbe15d2d6602fc613486bac292
sobuz <sobuz.info0298@gmail.com>
2024-09-08 01:19:15 +0600

    Complete user authentications

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import "./index.css";
 import { NextUIProvider } from "@nextui-org/system";
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <RouterProvider router={route} />
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <RouterProvider router={route} />
+        </PersistGate>
+      </NextUIProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logOut, setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
+import { TError } from "../../types/global";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
+// custom base query
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  const error = result?.error as TError;
+  if (error?.status === 404) {
+    toast.error(error?.data?.message);
+  }
+  // generate refresh token
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    if (data?.data?.accessToken) {
+      // set user and token
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(
+        setUser({
+          user,
+          token: data?.data?.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  endpoints: () => ({}),
+});
